59be90b68c12869039fcc6fe108f6a6696156907
jp <joao@ubots.com.br>
2020-04-08 10:03:34 -0300

    adding lodash dependency

--- src/components/generics/ITTags/ITTags.jsx
+++ src/components/generics/ITTags/ITTags.jsx
@@ -1,14 +1,15 @@
+import _ from 'lodash'
 import React from 'react';
 import { ITTag } from './ITTag'
 import './ITTags.scss'
 
+
 export const ITTags = (props) => {
-    const tags = props.tags || []
-    const elementTags = tags.map((tag, index) => <ITTag key={index} tagName={tag}/>)
+    const tags = _.map(props.tags, (tag, index) => <ITTag key={index} tagName={tag}/>)
 
     return (
         <div className="ITTags">
-            {elementTags}
+            {tags}
         </div>
     )
 }
