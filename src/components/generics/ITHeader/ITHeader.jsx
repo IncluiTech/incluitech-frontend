38852e3a70e627b782d6024b2065f2321092746d
alexiadorneles <lecavsalexia@gmail.com>
2020-04-07 23:39:35 -0300

    Remove unused function.

--- src/components/generics/ITHeader/ITHeader.jsx
+++ src/components/generics/ITHeader/ITHeader.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import Logo from '../../../assets/logo.png'
 import './ITHeader.scss'
 
-import Logo from '../../../assets/logo.png'
+
 
 export const ITHeader = () => {
 	return (
