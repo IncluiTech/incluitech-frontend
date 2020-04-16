99706f8d9e01ccef7d6127d15282e560773ad13a
alexiadorneles <lecavsalexia@gmail.com>
2020-04-15 23:58:44 -0300

    feat(client): load from backend in ClientView.

--- src/components/client/ClientView.jsx
+++ src/components/client/ClientView.jsx
@@ -1,47 +1,63 @@
-import React from 'react'
-import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
-import { Button, ButtonGroup } from '@material-ui/core';
+import {
+	Button,
+	ButtonGroup,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+} from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { clientService } from '../../services/client/clientService'
 import { ITTags } from '../generics'
-import ClientService from '../../services/client/clientService'
-
-
-const loadData = () => {
-    return ClientService.findClients()
-}
 
 const ClientView = () => {
-    return (
-        <TableContainer component={Paper} elevation={5}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell align="center">Nome</TableCell>
-                        <TableCell align="center">Email</TableCell>
-                        <TableCell align="center">Tags</TableCell>
-                        <TableCell align="center">Ações</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {loadData().map((row) => {
-                        return (<TableRow key={row.id}>
-                            <TableCell>{row.id}</TableCell>
-                            <TableCell align="center">{row.name}</TableCell>
-                            <TableCell align="center">{row.email}</TableCell>
-                            <TableCell align="center"><ITTags tags={row.tags} /></TableCell>
-                            <TableCell align="center">
-                                <ButtonGroup variant="contained">
-                                    <Button>Aprovar</Button>
-                                    <Button>Deletar</Button>
-                                </ButtonGroup>
-                            </TableCell>
-                        </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+	const [clients, updateClients] = useState([])
+	const findClients = async () => {
+		const clients = await clientService.findClients()
+		updateClients(clients)
+	}
+	useEffect(() => {
+		findClients()
+	}, [])
+
+	return (
+		<TableContainer component={Paper} elevation={5}>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>#</TableCell>
+						<TableCell align='center'>Nome</TableCell>
+						<TableCell align='center'>Email</TableCell>
+						<TableCell align='center'>Tags</TableCell>
+						<TableCell align='center'>Ações</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{clients.map(client => {
+						return (
+							<TableRow key={client.id}>
+								<TableCell>{client.id}</TableCell>
+								<TableCell align='center'>{client.nome}</TableCell>
+								<TableCell align='center'>{client.email}</TableCell>
+								<TableCell align='center'>
+									<ITTags tags={client.tags} />
+								</TableCell>
+								<TableCell align='center'>
+									<ButtonGroup variant='contained'>
+										<Button>Aprovar</Button>
+										<Button>Deletar</Button>
+									</ButtonGroup>
+								</TableCell>
+							</TableRow>
+						)
+					})}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	)
 }
 
-export { ClientView };
+export { ClientView }
