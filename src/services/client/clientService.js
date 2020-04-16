564da7a1d1b94be0c52de1cc60f90dd88b89bf52
alexiadorneles <lecavsalexia@gmail.com>
2020-04-15 23:58:27 -0300

    feat(service): change clientService to class.

--- src/services/client/clientService.js
+++ src/services/client/clientService.js
@@ -1,47 +1,9 @@
-import axios from 'axios'
-import config from '../../util/config'
+import { httpService } from '../httpService'
 
-const createData = (id, name, email, tags) => {
-    return { id, name, email, tags }
-}
-
-const mockedData = [
-    createData("1", "Pessoa1", "pessoa@mailing", ['tag', 'tag', 'tag', 'tag', 'tag']),
-    createData("2", "Pessoa2", "pessu@mailing", ['tag', 'tag', 'tag', 'tag', 'tag']),
-    createData("3", "Pessoa3", "people@mailing", ['tag', 'tag', 'tag', 'tag', 'tag', 'tag']),
-    createData("4", "Pessoa4", "person@mailing", ['tag', 'tag', 'tag', 'tag']),
-    createData("5", "Pessoa5", "vivente@mailing", ['tag', 'tag', 'tag'])
-]
-
-export default function () {
-    const httpRequest = axios.create({
-        baseURL: config.API_SSP_URL
-    })
-
-    const buildClientData = (clients) => {
-        return clients.map(client => {
-            return {
-                id: client.id,
-                name: client.name,
-                email: client.email,
-                tags: client.tags
-            }
-        })
-    }
-
-    const findClients = async () => {
-        try {
-            const response = await httpRequest().get("/v1/clients")
-            return buildClientData(response.data)
-        } catch (ex) {
-            console.error('Error Trying to get users in path /v1/clients', ex)
-            throw ex
-        }
-    }
-
-    return {
-        findClients: function () {
-            return mockedData
-        }
+class ClientService {
+    async findClients() {
+        return httpService.get("/v1/client")
     }
 }
+
+export const clientService = new ClientService()
