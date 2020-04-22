e5bc1df0150fe806ca8037f7dbcb475050386089
alexiadorneles <lecavsalexia@gmail.com>
2020-04-22 00:58:19 -0300

    Deixar solucionadores identico ao mockup.

--- src/components/screen/main/fragments/SolucionadorFragment.jsx
+++ src/components/screen/main/fragments/SolucionadorFragment.jsx
@@ -3,22 +3,62 @@ import { findClients } from '../../../../services/client/clientService'
 import { ITActions, ITTags } from '../../../generics'
 import { ITTable } from '../../../generics/ITTable/ITTable'
 
+/* !IMPORANTE!
+ * Esse código deve ser removido assim que a integração com o backend for feita
+ * TODO: Remover código
+ */
+const createData = (id, name, lattes, email, telefone, tags) => {
+  return { id, name, lattes, email, telefone, tags }
+}
+
+const mockedData = [
+  createData(
+    '1',
+    'Aléxia de Jesus Dorneles Pereira',
+    'http://lattes.cnpq.br/',
+    'alexia.pereira@edu.pucrs.br',
+    '(51) 98923-4000',
+    ['Escola', 'Empresa', 'Palestra', 'Adulto', 'Familia', 'Infantil'],
+  ),
+  createData(
+    '2',
+    'Gabriel Ferreira Kurtz',
+    'http://lattes.cnpq.br/',
+    'gabriel.kurtz@email.com',
+    '(51) 91234-1234',
+    ['Consultor', 'Conflitos', 'Empresa', 'Adulto'],
+  ),
+]
+
 export const SolucionadorFragment = () => {
   const [listSolucionador, setListSolucionador] = useState([])
   useEffect(() => {
-    const loadData = async () => {
-      setListSolucionador(await findClients())
-    }
-    loadData()
+    setListSolucionador(mockedData)
   }, [])
 
-  const tableHeader = ['#', 'Nome', 'Email', 'Tags', 'Ações']
+  const tableHeader = [
+    '#',
+    'Nome Completo',
+    'Curriculo Lattes',
+    'Email',
+    'Telefone',
+    'Tags',
+    'Ações',
+  ]
   const tableRowsConfig = [
     { prop: 'id' },
-    { prop: 'name' },
+    { prop: 'name', customStyle: { maxWidth: 50 } },
+    {
+      getCustomComponent: ({ lattes }) => () => (
+        <a target="blank" href={lattes}>
+          link
+        </a>
+      ),
+    },
     { prop: 'email' },
+    { prop: 'telefone' },
     { getCustomComponent: row => () => <ITTags tags={row.tags} /> },
-    { getCustomComponent: () => ITActions },
+    { getCustomComponent: () => ITActions, customStyle: { width: 175 } },
   ]
 
   return <ITTable data={listSolucionador} header={tableHeader} rowConfigs={tableRowsConfig} />
