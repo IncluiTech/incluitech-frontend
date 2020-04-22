455ca91fa0fecd28e2a98340e0700f13eb93af73
alexiadorneles <lecavsalexia@gmail.com>
2020-04-21 21:57:35 -0300

    fix(lint): fix lint in project.

--- src/components/client/ClientView.jsx
+++ src/components/client/ClientView.jsx
@@ -1,52 +1,60 @@
-import React, {useState,useEffect} from 'react'
-import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
-import { Button, ButtonGroup } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@material-ui/core'
+import { Button, ButtonGroup } from '@material-ui/core'
 import { ITTags } from '../generics'
-import { findClients} from '../../services/client/clientService'
-
-
-
+import { findClients } from '../../services/client/clientService'
 
 const ClientView = () => {
-    const [listClients,setListClients] = useState([])
-    useEffect(() => {
-         const loadData = async () => {
-            setListClients(await findClients()) 
-        }
-        loadData()
-    },[])
-    return (
-        <TableContainer component={Paper} elevation={5}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell align="center">Nome</TableCell>
-                        <TableCell align="center">Email</TableCell>
-                        <TableCell align="center">Tags</TableCell>
-                        <TableCell align="center">Ações</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {listClients.map((row) => {
-                        return (<TableRow key={row.id}>
-                            <TableCell>{row.id}</TableCell>
-                            <TableCell align="center">{row.name}</TableCell>
-                            <TableCell align="center">{row.email}</TableCell>
-                            <TableCell align="center"><ITTags tags={row.tags} /></TableCell>
-                            <TableCell align="center">
-                                <ButtonGroup variant="contained">
-                                    <Button>Aprovar</Button>
-                                    <Button>Deletar</Button>
-                                </ButtonGroup>
-                            </TableCell>
-                        </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+  const [listClients, setListClients] = useState([])
+  useEffect(() => {
+    const loadData = async () => {
+      setListClients(await findClients())
+    }
+    loadData()
+  }, [])
+  return (
+    <TableContainer component={Paper} elevation={5}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell align="center">Nome</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Tags</TableCell>
+            <TableCell align="center">Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {listClients.map(row => {
+            return (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell align="center">{row.name}</TableCell>
+                <TableCell align="center">{row.email}</TableCell>
+                <TableCell align="center">
+                  <ITTags tags={row.tags} />
+                </TableCell>
+                <TableCell align="center">
+                  <ButtonGroup variant="contained">
+                    <Button>Aprovar</Button>
+                    <Button>Deletar</Button>
+                  </ButtonGroup>
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
 
-export { ClientView };
+export { ClientView }
--- src/components/generics/ITHeader/ITHeader.jsx
+++ src/components/generics/ITHeader/ITHeader.jsx
@@ -3,16 +3,16 @@ import Logo from '../../../assets/logo.png'
 import './ITHeader.scss'
 
 export const ITHeader = () => {
-	return (
-		<div className='ITHeader'>
-			<div className='ITHeader__image--container'>
-				<img src={Logo} alt='IncluiTec Logo' className='ITHeader__image' />
-			</div>
-			<div className='ITHeader__ancoras'>
-				<a>Pendências</a>
-				<a>Estatísticas</a>
-				<a>Sair</a>
-			</div>
-		</div>
-	)
+  return (
+    <div className="ITHeader">
+      <div className="ITHeader__image--container">
+        <img src={Logo} alt="IncluiTec Logo" className="ITHeader__image" />
+      </div>
+      <div className="ITHeader__ancoras">
+        <a>Pendências</a>
+        <a>Estatísticas</a>
+        <a>Sair</a>
+      </div>
+    </div>
+  )
 }
