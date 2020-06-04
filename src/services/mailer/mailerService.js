528d417e427bf426336b96b480b51bb099ad7210
alexiadorneles <lecavsalexia@gmail.com>
2020-06-04 20:22:10 -0300

    refactor(prettier): run prettier in all files.

--- src/services/mailer/mailerService.js
+++ src/services/mailer/mailerService.js
@@ -2,25 +2,25 @@ import nodemailer from 'nodemailer'
 import config from '../../util/config'
 
 class MailerService {
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: '465',
-            secure: true,
-            auth: config.MAILLING_AUTH
-        })
-    }
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: '465',
+      secure: true,
+      auth: config.MAILLING_AUTH,
+    })
+  }
 
-    async sendMail(mailingContent) {
-        const mailContentProperty = mailingContent.body.isHtml ? 'html' : 'text'
-        const mail = {
-            from: `"" <${config.MAILING_AUTH.user}>`,
-            to: mailingContent.to,
-            subject: mailingContent.subject
-        }
-        mail[mailContentProperty] =  mailingContent.body.text
-        return this.transporter.sendMail(mail)
+  async sendMail(mailingContent) {
+    const mailContentProperty = mailingContent.body.isHtml ? 'html' : 'text'
+    const mail = {
+      from: `"" <${config.MAILING_AUTH.user}>`,
+      to: mailingContent.to,
+      subject: mailingContent.subject,
     }
+    mail[mailContentProperty] = mailingContent.body.text
+    return this.transporter.sendMail(mail)
+  }
 }
 
 export const mailerService = new MailerService()
