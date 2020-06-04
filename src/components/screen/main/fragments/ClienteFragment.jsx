a6166126f3160d753ad6df4c99a51c386f026e3c
alexiadorneles <lecavsalexia@gmail.com>
2020-06-04 20:22:00 -0300

    feat(newFields): add new fields in tables.

--- src/components/screen/main/fragments/ClienteFragment.jsx
+++ src/components/screen/main/fragments/ClienteFragment.jsx
@@ -3,35 +3,24 @@ import { clientService } from '../../../../services/client/clientService'
 import { ITActions, ITTags } from '../../../generics'
 import { ITTable } from '../../../generics/ITTable/ITTable'
 
-const createData = (id, name, email, tags) => {
-  return { id, name, email, tags }
-}
-
-
-const mockedData = [
-  createData("1", "Thomas", "thomas@mailing", ['Escola', 'Autismo']),
-  createData("2", "Rafael", "Rafael@mailing", ['Faculdade', 'Autismo']),
-  createData("3", "Carlos", "Carlos@mailing", ['ONG', 'Psicologia']),
-  createData("4", "Alexia", "Alexia@mailing", ['Trabalho', 'Foco', 'TDH']),
-  createData("5", "Joao Pedro", "joao@mailing", ['Convivencia', 'Colegas'])
-]
-
-
 export const ClienteFragment = () => {
   const [clients, updateClients] = useState([])
   const findClients = async () => {
-    const clients = await clientService.findClients();
+    const clients = await clientService.findClients()
     updateClients(clients)
   }
   useEffect(() => {
     findClients()
   }, [])
 
-  const tableHeader = ['#', 'Nome', 'Email', 'Tags', 'Ações']
+  const tableHeader = ['#', 'Nome', 'Email', 'Instituição', 'Função', 'Tags', 'Ações']
   const tableRowsConfig = [
     { prop: 'id' },
     { prop: 'nome', customStyle: { maxWidth: 50 } },
     { prop: 'email' },
+    { prop: 'instituicao' },
+    { prop: 'funcao' },
+
     { getCustomComponent: row => () => <ITTags tags={row.tags} /> },
     { getCustomComponent: () => ITActions, customStyle: { width: 175 } },
   ]
