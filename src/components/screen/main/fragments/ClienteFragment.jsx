7cd0bf33c13bfe934af4ff31da050ca76e5acb0e
alexiadorneles <lecavsalexia@gmail.com>
2020-06-09 23:13:58 -0300

    feat(problema): add modal into client view.

--- src/components/screen/main/fragments/ClienteFragment.jsx
+++ src/components/screen/main/fragments/ClienteFragment.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { clientService } from '../../../../services/client/clientService'
-import { ITActions, ITTags } from '../../../generics'
+import { ITActions, ITTags, ITModal } from '../../../generics'
 import { ITTable } from '../../../generics/ITTable/ITTable'
+import EditIcon from '@material-ui/icons/Edit'
+import Divider from '@material-ui/core/Divider'
 
 export const ClienteFragment = () => {
   const [clients, updateClients] = useState([])
+  const [selectedRow, setSelectedRow] = useState({})
+  const [isProblemModalOpen, setIsProblemModalOpen] = useState(false)
+
   const findClients = async () => {
     const clients = await clientService.findClients()
     updateClients(clients)
@@ -12,6 +17,20 @@ export const ClienteFragment = () => {
   useEffect(() => {
     findClients()
   }, [])
+
+  const getActions = model => (
+    <>
+      <ITActions model={model} />
+      <EditIcon
+        className="ITActions--container__icon"
+        onClick={() => {
+          setSelectedRow(model)
+          setIsProblemModalOpen(!isProblemModalOpen)
+        }}>
+        Editar
+      </EditIcon>
+    </>
+  )
 
   const tableHeader = ['#', 'Nome', 'Email', 'Instituição', 'Função', 'Tags', 'Ações']
   const tableRowsConfig = [
@@ -22,8 +41,31 @@ export const ClienteFragment = () => {
     { prop: 'funcao' },
 
     { getCustomComponent: row => () => <ITTags tags={row.tags} /> },
-    { getCustomComponent: () => ITActions, customStyle: { width: 175 } },
+    { getCustomComponent: row => () => getActions(row), customStyle: { width: 175 } },
   ]
 
-  return <ITTable data={clients} header={tableHeader} rowConfigs={tableRowsConfig} />
+  return (
+    <>
+      <ITTable data={clients} header={tableHeader} rowConfigs={tableRowsConfig} />
+      <ITModal
+        onClose={() => setIsProblemModalOpen(false)}
+        isOpen={isProblemModalOpen}
+        body={<ABC list={selectedRow.problemas} />}
+      />
+    </>
+  )
+}
+
+function ABC({ list }) {
+  return (
+    <div className="a">
+      <h1>PROBLEMAS: </h1>
+      {list.map((i, index) => (
+        <>
+          <p key={index}>{i}</p>
+          <Divider />
+        </>
+      ))}
+    </div>
+  )
 }
